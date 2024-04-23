529c375fb7b3a5c8dbdbcf661e7f697fffab28ed
LirijeShabani <LirijeShabani@LAPTOP-GRKV5HSB>
2024-04-23 13:10:23 +0200

    local sqlclient database implemented

--- SitemapGenerator/Program.cs
+++ SitemapGenerator/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SitemapGenerator.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
 
 var app = builder.Build();
 app.UseHsts();
