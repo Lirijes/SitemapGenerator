30750cbca935cf375cbdaa92a06bcf95fdf5f2b5
lirije <lirije11@hotmail.com>
2024-06-18 13:54:00 +0200

    removed live server configurations to see if they are the issue to deploy failure

--- bs-config.js
+++
@@ -1,8 +0,0 @@
-﻿// bs-config.js
-module.exports = {
-    proxy: "http://localhost:5085", // URL of your ASP.NET Core app
-    files: ["./wwwroot/**/*.{html,htm,css,js}", "./Views/**/*.cshtml"],
-    reloadDelay: 0,
-    notify: false,
-    open: false
-};
--- start-browser-sync.js
+++
@@ -1,9 +0,0 @@
-﻿const browserSync = require('browser-sync').create();
-
-browserSync.init({
-    proxy: "http://localhost:5085",
-    files: ["./wwwroot/**/*.{html,htm,css,js}", "./Views/**/*.cshtml"],
-    reloadDelay: 0,
-    notify: false,
-    open: false
-});
